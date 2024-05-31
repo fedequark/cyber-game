2433711357eb8df96be9c9f16922b54c7d6fc461
fedequark <federico.pacheco@gmail.com>
2024-05-31 09:49:20 +0200

    update

--- app.py
+++ app.py
@@ -7,19 +7,16 @@ app.secret_key = 'your_random_secret_key'
 # Cargar los datos del árbol de decisiones
 tree_data = load_tree_data('decision_tree.json')
 
-# Ruta para la nueva pantalla inicial
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Ruta original para la configuración del juego
 @app.route('/setup')
-def index():
+def setup():
     return render_template('setup.html')
 
 @app.route('/handle_setup', methods=['POST'])
 def handle_setup():
-    # Guardar los datos de configuración de la empresa en la sesión
     session['company'] = {
         'name': request.form['name'],
         'industry': request.form['industry'],
@@ -42,7 +39,6 @@ def player_setup():
 
 @app.route('/handle_player_setup', methods=['POST'])
 def handle_player_setup():
-    # Guardar los datos de configuración del jugador en la sesión
     session['player'] = {
         'name': request.form['player_name'],
         'role': request.form['player_role'],
@@ -50,12 +46,18 @@ def handle_player_setup():
         'technical_skills': request.form.getlist('technical_skills'),
         'non_technical_skills': request.form.getlist('non_technical_skills')
     }
-    return redirect(url_for('start_game'))
+    return redirect(url_for('summary'))
+
+@app.route('/summary')
+def summary():
+    company = session.get('company', {})
+    player = session.get('player', {})
+    return render_template('summary.html', company=company, player=player)
 
 @app.route('/start_game')
 def start_game():
-    session['report'] = [] # Iniciar o reiniciar el reporte
-    session['decisions'] = [] # Guardar las decisiones tomadas
+    session['report'] = []  # Iniciar o reiniciar el reporte
+    session['decisions'] = []  # Guardar las decisiones tomadas
     session['current_node'] = tree_data
     response = make_response(render_template('decision.html', node=tree_data))
     response.headers['Content-Type'] = 'text/html; charset=utf-8'
@@ -74,12 +76,12 @@ def handle_decision():
             else:
                 conclusion = option['conclusion']
                 session['report'].append(conclusion)
-                session.modified = True # Marcar la sesión como modificada
+                session.modified = True  # Marcar la sesión como modificada
                 return redirect(url_for('conclusion'))
             break
     if next_node:
         session['current_node'] = next_node
-        session.modified = True # Marcar la sesión como modificada
+        session.modified = True  # Marcar la sesión como modificada
         response = make_response(render_template('decision.html', node=next_node))
         response.headers['Content-Type'] = 'text/html; charset=utf-8'
         return response
@@ -88,7 +90,7 @@ def handle_decision():
 
 @app.route('/conclusion', methods=['GET'])
 def conclusion():
-    report = " ".join(session.get('report', [])) # Combinar todas las conclusiones
+    report = " ".join(session.get('report', []))  # Combinar todas las conclusiones
     decisions = session.get('decisions', [])
     response = make_response(render_template('conclusion.html', message=report, decisions=decisions))
     response.headers['Content-Type'] = 'text/html; charset=utf-8'
