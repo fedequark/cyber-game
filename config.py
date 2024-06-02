7d5bda78e7ddc655e51088a0aec314cba99dd071
fedequark <federico.pacheco@gmail.com>
2024-06-02 12:41:15 +0200

    update

--- config.py
+++ config.py
@@ -2,13 +2,18 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SESSION_TYPE = 'filesystem'
+    USE_DATABASE = os.environ.get('USE_DATABASE', 'false').lower() in ['true', '1', 't']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///development.db'
+    USE_DATABASE = False  # Cambiar a True para usar el workflow basado en la base de datos
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///production.db'
+    USE_DATABASE = False  # Cambiar a True para usar el workflow basado en la base de datos
 
 config = {
     'development': DevelopmentConfig,
